fa3441688ca99219a99714b6cd2c2a343d73b8d2
Colin Caine <cmc@cs.man.ac.uk>
2017-11-26 16:03:23 +0000

    remove comment

--- DutchNumber.java
+++ DutchNumber.java
@@ -1,8 +1,6 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-// TODO: Write some real tests, then implement the match method below to get the tests to pass.
-
 public class DutchNumber implements PhoneValidator {
 
     public boolean isValid(String maybePhone) {
